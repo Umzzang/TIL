9f25d05318e3e282733a624d0626f83b2408ea22
YoonKyu <newjeff1993@gmail.com>
2022-07-14 14:44:56 +0900

    JS-random

--- JavaScript/Clock/clock.js
+++ JavaScript/Clock/clock.js
@@ -1,14 +1,51 @@
 
 const clock = document.getElementById("clock")
+const wantDate = document.getElementById("d")
 
-function setClock () {
-  const date = new Date()
-  const day = String(date.getDate().padStart(3,"0"))
-  const hour = String(date.getHours().padStart(2,"0"))
-  const minute = String(date.getMinutes().padStart(2,"0"))
-  const second = String(date.getSeconds().padStart(2,"0"))
+let check = 0
+
+function setDate (event) {
+  const wishDate = event.target.valueAsDate
+  const nowDate = new Date()
+  let difDay = wishDate - nowDate
+  const day = parseInt(difDay/(1000 * 60 * 60 * 24))
+  difDay = difDay % (1000 * 60 * 60 * 24)
+  const hour = parseInt(difDay/(1000 * 60 * 60))
+  difDay = difDay % (1000 * 60 * 60)
+  const minute = parseInt(difDay/(1000 * 60 ))
+  difDay = difDay % (1000 * 60 )
+  const second = parseInt(difDay/(1000 ))
+  difDay = difDay % (1000 )
+  
   clock.innerText = `${day}d ${hour}h ${minute}m ${second}s`
+  check = 1
 }
 
-setClock()
-setInterval(setClock, 500)
+
+
+wantDate.addEventListener("change", setDate)
+
+function setDate2 () {
+  if (check === 1) {
+
+    const wishDate = wantDate.valueAsDate
+    const nowDate = new Date()
+    let difDay = wishDate - nowDate
+    const day = parseInt(difDay/(1000 * 60 * 60 * 24))
+    difDay = difDay % (1000 * 60 * 60 * 24)
+    const hour = parseInt(difDay/(1000 * 60 * 60))
+    difDay = difDay % (1000 * 60 * 60)
+    const minute = parseInt(difDay/(1000 * 60 ))
+    difDay = difDay % (1000 * 60 )
+    const second = parseInt(difDay/(1000 ))
+    difDay = difDay % (1000 )
+    
+    clock.innerText = `${day}d ${hour}h ${minute}m ${second}s`
+}
+  
+}
+
+
+setInterval(setDate2, 500)
+
+
